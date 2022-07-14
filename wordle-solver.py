d226fe8c22c1c77bd4ea0a51d708870969e67704
Alan <70598434+alwritescode@users.noreply.github.com>
2022-07-14 15:42:52 -0700

    debug & added skip/manual guessing
    Fixed bug with duplicate letter guesses. Came across multiple incompatible & missing words in vocab library; should either modify or replace this library. Added option to skip words and guess manually.

--- wordle-solver.py
+++ wordle-solver.py
@@ -2,7 +2,7 @@
 # start with a word containing as many of the most common letters as possible (using a library indexing letters by a value of common-ness)
 # pick each next word based on option with highest common-ness sum of letters, which also fulfills existing restraints
 
-from english_words import english_words_lower_alpha_set as allWords
+from english_words import english_words_lower_alpha_set
 from string import ascii_lowercase as alpha
 import ast
 from copy import deepcopy
@@ -11,34 +11,34 @@ from copy import deepcopy
 ALPHAD = {letter: [] for letter in alpha}
 
 # get only 5-letter words
-vocab = []
-for word in allWords:
+VOCAB = []
+for word in english_words_lower_alpha_set:
     if '.' in word:  # because for some reason this library includes 'U.S.A'
         pass
     elif len(word) == 5:
-        vocab.append(word)
+        VOCAB.append(word)
 
 # set up letter frequency dict & fill dictionary with data
-alphaFreqs = {letter: [0, 0, 0, 0, 0] for letter in alpha}
-for word in vocab:
+letterCounts = {letter: [0, 0, 0, 0, 0] for letter in alpha}
+for word in VOCAB:
     for i in range(5):
         letter = word[i]
-        alphaFreqs[letter][i] +=1
+        letterCounts[letter][i] +=1
 
 # rank words by common-ness of letters
-scores = {}
-for word in vocab:
+SCORES = {}
+for word in VOCAB:
     thisScore = 0
     for i in range(5):
         letter = word[i]
-        thisScore += alphaFreqs[letter][i]
-    scores[word] = thisScore
+        thisScore += letterCounts[letter][i]
+    SCORES[word] = thisScore
 
 # core function to pick optimal word
 def guess(green, yellow, excludes):
     # green are dictionaries of {letter: [position, ...]}. Excluded is list of just [letter, ...]
     disqualifiedWords = []  # to avoid modifying loop while for-looping over it
-    for word in vocab:
+    for word in VOCAB:
         for letter in green:
             for pos in green[letter]:
                 if word[pos] != letter:
@@ -52,14 +52,23 @@ def guess(green, yellow, excludes):
                         disqualifiedWords.append(word)
         for letter in excludes:
             if letter in word:
-                disqualifiedWords.append(word)
+                if letter in green:  # deals with excluded duplicate letters (single-use letters)
+                    greenCount = len(green[letter])
+                    letterCount = word.count(letter)
+                    if letterCount > greenCount:
+                        disqualifiedWords.append(word)
+                else:
+                    disqualifiedWords.append(word)
     # reconfigure list
     disqualifiedWords = list(set(disqualifiedWords))  # removes duplicates
     for word in disqualifiedWords:
-        vocab.remove(word)
-        del scores[word]
+        VOCAB.remove(word)
+        del SCORES[word]
     # return both the best guess (highest scoring qualifying word), and the number of possible guesses
-    return max(scores, key=scores.get), len(vocab)
+    try:
+        return max(SCORES, key=SCORES.get), len(VOCAB)
+    except ValueError:
+        return '', 0
 
 def parseResults(results, prevGuess):
     green, yellow = deepcopy(ALPHAD), deepcopy(ALPHAD)
@@ -77,12 +86,25 @@ def parseResults(results, prevGuess):
     return green, yellow, excludes
 
 # program body
-print('Instructions: Enter the results from each guess in the format of _ _ _ _ _, with Y (yellow) or G (green).')
-prevGuess = guess([], [], [])[0]
+print('Instructions: Enter the results from each guess in the format of "_ _ _ _ _", with "Y" (yellow) or "G" (green) in place of underscores. Or, enter "skip" to disqualify a word, or "all" to see all possible guesses.')
+green, yellow, excludes = {}, {}, []
+prevGuess = guess(green, yellow, excludes)[0]
 print('Starting word:', prevGuess)  # first guess should be "SAUTE"
 
-for i in range(1, 5):  # start loop at second try (1)
+for i in range(1, 6):  # start loop at second try (1), 6 guesses total
     guessResults = input('Results? -> ')
+    while guessResults == 'skip':
+        VOCAB.remove(prevGuess)
+        del SCORES[prevGuess]
+        newGuess = guess(green, yellow, excludes)[0]  # retroactively
+        print('Skipped '+prevGuess+'. Try:', newGuess)
+        prevGuess = newGuess
+        guessResults = input('Results? -> ')
+    while guessResults == 'all':
+        print(', '.join(VOCAB))
+        prevGuess = input('Which word did you guess? -> ')
+        guessResults = input('Results? -> ')
+    # following regular guess input, "_ _ etc":
     green, yellow, excludes = parseResults(guessResults, prevGuess)
     currentGuess, optionCount = guess(green, yellow, excludes)
     print(optionCount, 'matching word' + 's'*(optionCount!=1), 'found.', end=' ')
